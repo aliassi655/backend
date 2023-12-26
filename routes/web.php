3d630842a63d6962bf7530fbd30c4c6c405cf25f
ali assi <azassi655@gmail.com>
2023-12-26 17:21:35 +0300

    adding login view and dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/*Route::get('company/add',
+    [\App\Http\Controllers\CompanyController::class,'add'])
+    ->middleware('auth')
+    ->name('company.add');*/
